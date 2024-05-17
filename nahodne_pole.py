49112550f1e321beb97c889385b5f5ecb4b83883
GustavFlachs <152276245+GustavFlachs@users.noreply.github.com>
2024-05-17 08:44:35 +0200

    Add files via upload

---
+++ nahodne_pole.py
@@ -0,0 +1,16 @@
+import random
+
+cisla_1 = []
+cisla_2 = []
+spolu_cisla = []
+
+for x in range(5):
+    cisla_1.append(random.randint(0,10))
+    
+print("pole_1:",cisla_1)
+
+for x in range(5):
+    cisla_2.append(random.randint(0,10))
+
+print("pole_2:",cisla_2)
+
